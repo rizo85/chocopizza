0059fb2d4268ffe0ad1b4720d6ff15a6c08b7af2
rizo85r <rizo85r@gmail.com>
2020-06-15 03:14:52 -0700

    All of it is now done

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+/* 
+MARK PALFREEMAN
+CODE FELLOWS 
+UX BOOTCAMP APPLICATION
+*/
+
+$(document).ready(function() {
+  $(".ingredients li").click(function () {
+     $(this).toggleClass("checked");
+  });
+});
+
